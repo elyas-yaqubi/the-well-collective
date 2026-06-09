8920013bfffea7c49787734cb9a955bdea0a3e66
Elyas Yaqubi <elyasyaqubi13@gmail.com>
2026-06-09 00:58:31 -0700

    Removed template code and added title
    Added "The Well Collective" to homepage

--- studio-well-in-the-village/sanity.cli.ts
+++ studio-well-in-the-village/sanity.cli.ts
@@ -6,6 +6,7 @@ export default defineCliConfig({
     dataset: 'production'
   },
   deployment: {
+    appId: 'xaptzp06qgnhe18u2aofy38f',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
